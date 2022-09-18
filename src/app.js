815c49b51d9de97c38a020972448028ca93bbbdd
Ali Faizan <alifaizan1983@gmail.com>
2022-09-18 21:34:43 +0530

    added some features and modified some files

--- src/app.js
+++ src/app.js
@@ -21,16 +21,15 @@ let quiz = {
   quizData: function () {
     fetch(
       "https://the-trivia-api.com/api/questions?limit=10&region=IN&difficulty=easy"
-      )
+    )
       .then((res) => res.json())
-      .then((data) => this.diplayQuestions(data))
-    },
-    
-    diplayQuestions: function (data) {
-      const { question } = data[0];
-      const { correctAnswer } = data[0];
-      const { incorrectAnswers } = data[0];
+      .then((data) => this.diplayQuestions(data));
+  },
 
+  diplayQuestions: function (data) {
+    const { question } = data[0];
+    const { correctAnswer } = data[0];
+    const { incorrectAnswers } = data[0];
 
     let opt1 = incorrectAnswers[0];
     let opt2 = incorrectAnswers[1];
@@ -59,60 +58,209 @@ let quiz = {
     o3.innerText = randomOptions[2];
     o4.innerText = randomOptions[3];
 
+    // console.log(correctAnswer);
+
     o1.addEventListener("click", function () {
       if (o1.innerText == correctAnswer) {
-        o1.style.backgroundColor = "green";
+        o1.style.backgroundColor = "#0BDA51";
         o1.style.color = "white";
         score++;
       } else {
-        o1.style.backgroundColor = "red";
+        o1.style.backgroundColor = "#8B0000";
         o1.style.color = "white";
       }
       o2.style.pointerEvents = "none";
       o3.style.pointerEvents = "none";
       o4.style.pointerEvents = "none";
+
+      // if(o1.innerText != correctAnswer){
+      //   let k = correctAnswer;
+      //   for(let j = 0; j < randomOptions.length; j++){
+      //     if(k == randomOptions[j]){
+      //       console.log(correctAnswer);
+      //       console.log(j);
+      //       console.log(o1.innerText);
+
+      //     }
+      //   }
+      // }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText == correctAnswer
+      ) {
+        o4.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText == correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o3.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText == correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o2.style.backgroundColor = "#0BDA51";
+      }
     });
 
     o2.addEventListener("click", function () {
       if (o2.innerText == correctAnswer) {
-        o2.style.backgroundColor = "green";
+        o2.style.backgroundColor = "#0BDA51";
         o2.style.color = "white";
         score++;
       } else {
-        o2.style.backgroundColor = "red";
+        o2.style.backgroundColor = "#8B0000";
         o2.style.color = "white";
       }
       o1.style.pointerEvents = "none";
       o3.style.pointerEvents = "none";
       o4.style.pointerEvents = "none";
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText == correctAnswer
+      ) {
+        o4.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText == correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o3.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText == correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o2.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText == correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o1.style.backgroundColor = "#0BDA51";
+      }
     });
 
     o3.addEventListener("click", function () {
       if (o3.innerText == correctAnswer) {
-        o3.style.backgroundColor = "green";
+        o3.style.backgroundColor = "#0BDA51";
         o3.style.color = "white";
         score++;
       } else {
-        o3.style.backgroundColor = "red";
+        o3.style.backgroundColor = "#8B0000";
         o3.style.color = "white";
       }
       o2.style.pointerEvents = "none";
       o1.style.pointerEvents = "none";
       o4.style.pointerEvents = "none";
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText == correctAnswer
+      ) {
+        o4.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText == correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o3.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText == correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o2.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText == correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o1.style.backgroundColor = "#0BDA51";
+      }
     });
 
     o4.addEventListener("click", function () {
       if (o4.innerText == correctAnswer) {
-        o4.style.backgroundColor = "green";
+        o4.style.backgroundColor = "#0BDA51";
         o4.style.color = "white";
         score++;
       } else {
-        o4.style.backgroundColor = "red";
+        o4.style.backgroundColor = "#8B0000";
         o4.style.color = "white";
       }
       o2.style.pointerEvents = "none";
       o3.style.pointerEvents = "none";
       o1.style.pointerEvents = "none";
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText == correctAnswer
+      ) {
+        o4.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText == correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o3.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText != correctAnswer &&
+        o2.innerText == correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o2.style.backgroundColor = "#0BDA51";
+      }
+
+      if (
+        o1.innerText == correctAnswer &&
+        o2.innerText != correctAnswer &&
+        o3.innerText != correctAnswer &&
+        o4.innerText != correctAnswer
+      ) {
+        o1.style.backgroundColor = "#0BDA51";
+      }
     });
 
     o1.style.pointerEvents = "auto";
@@ -120,8 +268,6 @@ let quiz = {
     o3.style.pointerEvents = "auto";
     o4.style.pointerEvents = "auto";
   },
-
-
 };
 
 // quiz.
@@ -132,7 +278,7 @@ let op4 = document.querySelector(".op4");
 
 let interval = setInterval(() => {
   quiz.quizData();
-  
+
   op1.style.backgroundColor = "white";
   op1.style.color = "black";
 
@@ -149,12 +295,27 @@ let interval = setInterval(() => {
 
 setTimeout(() => {
   clearInterval(interval);
+
+  op1.innerHTML  = "";
+  op2.innerText  = "";
+  op3.innerText  = "";
+  op4.innerText  = "";
+
   let thescore = score;
   let finalscore = document.getElementById("finalscore");
   finalscore.append(thescore);
-  console.log(thescore)
-  
-  
+
+  alert(`It's over, check your score!`);
+
+
+
+
 }, 100000);
 
 quiz.quizData();
+
+//play again button
+const playAgain = document.querySelector(".play-again");
+playAgain.addEventListener("click", () => {
+  window.location.reload();
+});
